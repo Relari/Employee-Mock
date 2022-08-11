5d2a191c6688fd42d91ac13fb79d60125cbf73dc
Renzo Daniel Lavado Rivas <renzo.lavado@pragma.com.co>
2022-08-11 12:29:54 -0500

    test: updated unit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,15 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
 	private DemoController demoController;
 
-	@BeforeEach
-	void init() {
-		demoController = new DemoController();
-	}
+	// @BeforeEach
+	// void init() {
+	// 	demoController = new DemoController();
+	// }
 
 	@Test
 	void contextLoads() {
