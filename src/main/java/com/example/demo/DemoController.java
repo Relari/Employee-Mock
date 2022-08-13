9560fc1a470a775b1c6b3879d20ccdaed05983bf
Relari <renzolavador@gmail.com>
2022-08-13 00:51:10 -0500

    add fix

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class DemoController {
